2fae79b812170e0d234593f069fe729ff37395ed
udaya0205 <125043102+udaya0205@users.noreply.github.com>
2023-02-14 15:31:15 +0530

    Update Demo.java

--- src/main/java/OraniumTech/SimpleProject/Demo.java
+++ src/main/java/OraniumTech/SimpleProject/Demo.java
@@ -1,5 +1,12 @@
 package OraniumTech.SimpleProject;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo {
-
+  public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	    driver.get("https://www.facebook.com/");
+	    Thread.sleep(3000);
+	    //driver.close();
+  }
 }
